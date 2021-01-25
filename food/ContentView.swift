a876562886344785599c8b1d4bec2d0f344b46f1
Abu Anwar <abuanwar072@gmail.com>
2021-01-25 15:10:53 +0600

    Initial Commit

---
+++ food/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  food
+//
+//  Created by Abu Anwar MD Abdullah on 25/1/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ food/foodApp.swift
@@ -0,0 +1,17 @@
+//
+//  foodApp.swift
+//  food
+//
+//  Created by Abu Anwar MD Abdullah on 25/1/21.
+//
+
+import SwiftUI
+
+@main
+struct foodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
